c2c796ccd7fe3fd3fdf30d6e65a7e9ae4d94f548
Jim Boulter <jboulter11@gmail.com>
2021-01-14 22:11:18 -0800

    Initial Commit

---
+++ HifiGuides/HifiGuidesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HifiGuidesApp.swift
+//  HifiGuides
+//
+//  Created by James Boulter on 1/14/21.
+//
+
+import SwiftUI
+
+@main
+struct HifiGuidesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
